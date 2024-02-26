16da6b73ad49415ead3637f79addedf91b0f53e4
Bisma Gondal <159830145+Bisma786@users.noreply.github.com>
2024-02-26 22:16:14 +0500

    Add files via upload

---
+++ ex36.js
@@ -0,0 +1,12 @@
+"use strict";
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+const make_shirt = (size, message) => {
+    console.log(`Size: ${size}, Message:'${message}'`);
+    return `Size:${size}, Message: '${message}'`;
+};
+let size = "Large";
+let message = "Bisma Digitalisma";
+// make_shirt("Medium","Bisma Digitalisma");ing="medium";
+make_shirt("Medium", "Bisma Digitalisma");
+make_shirt(size, message);
+// 
